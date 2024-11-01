547fb498c4ff75a2156007e12ae341adac3eb70d
taskinnasif144 <taskinnasif144@gmail.com>
2024-11-02 00:27:06 +0600

    footer ready

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,8 +8,8 @@ import { Instagram, YouTube } from '@mui/icons-material';
 
 const Footer = () => {
     return (
-        <div className='bg-black text-white p-16 md:px-32'>
-            <div>
+        <div className='bg-black text-white p-16 md:px-32 flex flex-wrap justify-between'>
+            <div className='my-4'>
                 <h3 className='text-2xl font-bold mb-6'>Company Info</h3>
                 <h5>Satisfaction Guarantee</h5>
                 <h5>Privacy Policy</h5>
@@ -24,7 +24,27 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div></div>
+            <div className='my-4'>
+                <h3 className='text-2xl font-bold mb-6'>Service Area</h3>
+                <div className='my-2'><h5>Massachusetts </h5>
+                    <h5><span className='font-bold'>Address: </span> 55 KILBY ST, BOSTON, MA 02109</h5></div>
+                <div className='my-2'>     <h5>
+                    Maryland </h5>
+                    <h5><span className='font-bold'>Address: </span>904 S BROADWAY, BALTIMORE, MD 21231</h5></div>
+
+
+                <h4 className='my-2'><span className='font-bold'>CONTACT: </span> 214 329 9114</h4>
+            </div>
+            <div className='my-4'>
+                <h3 className='text-2xl font-bold mb-6'>Hours of Operation</h3>
+                <h4><span className='font-bold'>Monday:</span> 8:00AM-9:00PM</h4>
+                <h4><span className='font-bold'>Tuesday:</span> 8:00AM-9:00PM</h4>
+                <h4><span className='font-bold'>Wednesday:</span> 8:00AM-9:00PM</h4>
+                <h4><span className='font-bold'>Thursday:</span> 8:00AM-9:00PM</h4>
+                <h4><span className='font-bold'>Friday:</span> 8:00AM-8:00PM</h4>
+                <h4><span className='font-bold'>Saturday:</span> 8:00AM-8:00PM</h4>
+                <h4><span className='font-bold'>Sunday:</span> 8:00AM-8:00PM</h4>
+            </div>
         </div>
     )
 }
--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -9,12 +9,12 @@ import 'react-phone-input-2/lib/style.css'
 const Quote = () => {
     const [phone, setPhone] = useState("")
     return (
-        <section id='Quote' className='relative text-white p-6 my-6' >
+        <section id='Quote' className='relative text-white p-6 my-6 flex flex-col items-center' >
             <Image alt='backgrod' src={"/office.jpg"} fill={true} className='object-cover -z-10 blur-sm' />
             <h3 className='text-3xl font-bold mb-12'>Get a Free Quote Today!</h3>
-            <form action="" className='flex flex-col gap-4'>
-                <input type="text" placeholder='Full Name' className='px-3 py-1 rounded-sm w-full focus:outline-none text-black' />
-                <input type="email" placeholder='Email' className='px-3 py-1 rounded-sm w-full focus:outline-none text-black' />
+            <form action="" className='flex flex-col gap-4 w-full max-w-[550px]'>
+                <input type="text" placeholder='Full Name' className='px-3 py-1 rounded-md w-full focus:outline-none text-black ' />
+                <input type="email" placeholder='Email' className='px-3 py-1 rounded-md w-full focus:outline-none text-black' />
                 <div className='text-black'>
                     <PhoneInput
                         inputStyle={{ width: '100%' }}
@@ -25,8 +25,8 @@ const Quote = () => {
 
                     />
                 </div>
-                <input type="text" placeholder='Zipcode' className='px-3 py-1 rounded-sm w-full focus:outline-none text-black' />
-                <textarea name="" id="" placeholder='Message....' className='px-3 py-1 rounded-sm w-full focus:outline-none text-black'></textarea>
+                <input type="text" placeholder='Zipcode' className='px-3 py-1 rounded-md w-full focus:outline-none text-black' />
+                <textarea name="" id="" placeholder='Message....' className='px-3 py-1 rounded-md w-full focus:outline-none text-black'></textarea>
                 <button className='bg-color-sidebar py-1 rounded-2xl'>Submit</button>
             </form>
         </section>

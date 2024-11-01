19ae08476d4eef660cb999a92be15330bfecf441
taskinnasif144 <taskinnasif144@gmail.com>
2024-11-02 03:24:09 +0600

    text-start

--- src/components/GetUserInfo.tsx
+++ src/components/GetUserInfo.tsx
@@ -1,23 +1,36 @@
 'use client'
 
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { FormEvent, useState } from 'react'
 import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/style.css'
 
 
 
 const GetUserInfo = () => {
-    const [phone, setPhone] = useState('')
+    const [phone, setPhone] = useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [dropdown, setDropdown] = useState('');
+
+    const formSubmition = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(phone);
+        console.log(name);
+        console.log(email);
+        console.log(dropdown);
+
+    }
+
     return (
         <div className='absolute  w-full lg:w-4/5'>
             <div className='relative p-6 rounded flex flex-col gap-6 justify-center items-center '>
                 <Image alt='background image' src={"/firstForm.jpeg"} fill={true} className='-z-10 object-cover brightness-75 rounded-2xl' />
                 <h3 className='text-white font-bold  '>Get A Free Quote Now</h3>
-                <form className=' flex flex-col justify-center items-center'>
+                <form className=' flex flex-col justify-center items-center' onSubmit={formSubmition}>
                     <div className='flex items-center  gap-4 flex-col md:flex-row'>
-                        <input type="text" placeholder='Full Name' className='px-4 py-1 rounded w-[200px] focus:outline-none' />
-                        <input type="email" placeholder='Email' className='px-4 py-1 rounded  w-[200px] focus:outline-none' />
+                        <input type="text" placeholder='Full Name' className='px-4 py-1 rounded w-[200px] focus:outline-none' value={name} onChange={(e) => setName(e.target.value)} />
+                        <input type="email" placeholder='Email' className='px-4 py-1 rounded  w-[200px] focus:outline-none' onChange={e => setEmail(e.target.value)} />
                         <PhoneInput
                             inputStyle={{ width: '210px' }}
                             country={'us'}
@@ -26,10 +39,10 @@ const GetUserInfo = () => {
                             inputClass='w-full'
 
                         />
-                        <select className='w-[210px] text-sm outline-none appearance-none text-center px-3 py-2'>
-                            <option value="" className='focus:outline-none '>Air Duct & Ventilation Cleaning</option>
-                            <option value="" className='focus:outline-none px-3 py-2'>Chimney Fireplaces Cleaning</option>
-                            <option value="" className='focus:outline-none px-3 py-2'>Dryer Vent Cleaning</option>
+                        <select className='w-[210px] text-sm outline-none appearance-none text-start px-3 py-2' onChange={(e) => { setDropdown(e.target.value) }}>
+                            <option value="Air Duct & Ventilation Cleaning" className='focus:outline-none '>Air Duct & Ventilation Cleaning</option>
+                            <option value="Chimney Fireplaces Cleaning" className='focus:outline-none px-3 py-2'>Chimney Fireplaces Cleaning</option>
+                            <option value="Dryer Vent Cleaning" className='focus:outline-none px-3 py-2'>Dryer Vent Cleaning</option>
                         </select>
                     </div>
                     <button className=' text-white bg-color-sidebar-secondary px-3 py-1 rounded-lg mt-6'>Submit Now</button>
--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -7,14 +7,29 @@ import 'react-phone-input-2/lib/style.css'
 
 
 const Quote = () => {
-    const [phone, setPhone] = useState("")
+    const [phone, setPhone] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [Zipcode, setZipcode] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleFormSubmission = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(phone);
+        console.log(name);
+        console.log(email);
+        console.log(Zipcode);
+        console.log(message);
+
+    }
+
     return (
         <section id='Quote' className='relative text-white p-6 my-6 flex flex-col items-center' >
             <Image alt='backgrod' src={"/office.jpg"} fill={true} className='object-cover -z-10 blur-sm' />
             <h3 className='text-3xl font-bold mb-12'>Get a Free Quote Today!</h3>
-            <form action="" className='flex flex-col gap-4 w-full max-w-[550px]'>
-                <input type="text" placeholder='Full Name' className='px-3 py-1 rounded-md w-full focus:outline-none text-black ' />
-                <input type="email" placeholder='Email' className='px-3 py-1 rounded-md w-full focus:outline-none text-black' />
+            <form action="" className='flex flex-col gap-4 w-full max-w-[550px]' onSubmit={handleFormSubmission}>
+                <input type="text" placeholder='Full Name' className='px-3 py-1 rounded-md w-full focus:outline-none text-black ' value={name} onChange={e => setName(e.target.value)} />
+                <input type="email" placeholder='Email' className='px-3 py-1 rounded-md w-full focus:outline-none text-black' value={email} onChange={e => setEmail(e.target.value)} />
                 <div className='text-black'>
                     <PhoneInput
                         inputStyle={{ width: '100%' }}
@@ -25,9 +40,9 @@ const Quote = () => {
 
                     />
                 </div>
-                <input type="text" placeholder='Zipcode' className='px-3 py-1 rounded-md w-full focus:outline-none text-black' />
-                <textarea name="" id="" placeholder='Message....' className='px-3 py-1 rounded-md w-full focus:outline-none text-black'></textarea>
-                <button className='bg-color-sidebar py-1 rounded-2xl'>Submit</button>
+                <input type="text" placeholder='Zipcode' className='px-3 py-1 rounded-md w-full focus:outline-none text-black' value={Zipcode} onChange={e => setZipcode(e.target.value)} />
+                <textarea name="" id="" placeholder='Message....' className='px-3 py-1 rounded-md w-full focus:outline-none text-black' value={message} onChange={e => setMessage(e.target.value)}></textarea>
+                <button type='submit' className='bg-color-sidebar py-1 rounded-2xl'>Submit</button>
             </form>
         </section>
     )
